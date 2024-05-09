02ea834b3bbb1264ad1951293624c130bf25c313
unknown <keane.natanael@gmail.com>
2024-05-09 11:07:32 +0700

    .

--- platformer.py
+++ platformer.py
@@ -1,3 +1,14 @@
-from PIL import Image
-with Image.open("assets/7 Levels/Preview/2lvl.jpg") as original:
-    original.show()
+from pygame import *
+window = display.set_mode((1280, 720))
+display.set_caption("Platformer Game")
+background = transform.scale(image.load("assets/7 Levels/Preview/2lvl.jpg"), (1280, 720))
+
+game = True
+while game:
+    window.blit(background, (0, 0))
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+    display.update()
